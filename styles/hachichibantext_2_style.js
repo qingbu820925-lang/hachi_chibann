47cd7ec44c4db28a276bbb88f3c9539f202079da
qingbu820925-lang <qingbu820925@gmail.com>
2026-01-02 08:06:11 +0900

    Add files via upload

---
+++ styles/hachichibantext_2_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_hachichibantext_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("''");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("azachiban") !== null) {
+        labelText = String(feature.get("azachiban"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+
+    return style;
+};
